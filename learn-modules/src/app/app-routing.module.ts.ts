b970a46aa4843aefae34973915a370d003f54eb1
Zaid-Mone <zaidmone9@gmail.com>
2024-02-10 14:11:04 +0300

    add-error-component

--- learn-modules/src/app/app-routing.module.ts.ts
+++ learn-modules/src/app/app-routing.module.ts.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './guards/auth.guard';
+import { Error404Component } from './components/error404/error404.component';
 
 
 const routes: Routes = [
+  { path: '**', component: Error404Component, title:'Error Page' }
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, title: 'Home page' },
   { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(a => a.AdminModule), canActivate: [authGuard] }
--- learn-modules/src/app/app.module.ts
+++ learn-modules/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module.ts';
+import { Error404Component } from './components/error404/error404.component';
 //import { AdminModule } from './modules/admin/admin.module';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module.ts';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
+    Error404Component,
      //AdminModule
   ],
   imports: [
